1a28b8ee097f8a87c69e9a23026288c19d1ce849
youhane <jason.sebastian154@gmail.com>
2022-03-07 10:31:43 +0700

    reverted

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Box } from "@mui/material";
 function App() {
   return (
     <Box sx={{ bgcolor:'primary.main' }}>
-      <h1>Country Quiz</h1>
+      <h1>Country Quiaz</h1>
     </Box>
   );
 }
